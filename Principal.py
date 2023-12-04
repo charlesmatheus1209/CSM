acadf647fa4705b3a3632714ec9bf63749bd090e
charlesmatheus1209 <charlesmatheus1209@gmail.com>
2023-12-04 15:55:21 -0300

    Funcoes foram realocadas para o arquivo Controle.py

--- Principal.py
+++ Principal.py
@@ -3,24 +3,8 @@ from Display import *
 from Controle import *
 from InterfaceWeb import *
 import time
-import threading
-import random
 
-def QuantidadeDePulsos():
-    p = random.randrange(0, 200)
-    print("Pulsos: ", p)
-    return p
 
-def CalculaVazao(pulsos, intervaloReal):
-    # Frequencia de pulsos = 6,6 * Vazão
-    # Vazão = Frequencia/6.6
-    frequencia = (1/ intervaloReal) * pulsos
-    print("Frequencia: ", frequencia, "pulsos/seg")
-    
-    vazao = frequencia/6.6
-    print("Vazão: ", vazao, "L/min")
-    return vazao
-    
     
     
     
